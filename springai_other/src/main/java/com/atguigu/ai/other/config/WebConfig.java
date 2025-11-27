113306ebe524db76cd59f86928645a5b30aa68f7
action <xingdong1117@126.com>
2025-11-27 09:07:22 +0800

    feat: 图像和语音模型 测试路径相关

---
+++ springai_other/src/main/java/com/atguigu/ai/other/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.atguigu.ai.other.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/tts/**")
+                .addResourceLocations("classpath:/tts/");
+    }
+}
